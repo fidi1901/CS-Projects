48abec22d7a46d16232871f38f4715c3d7831d13
Farhan <farahanidris00@gmail.com>
2022-06-06 00:00:26 -0500

    Simple Gradebook Project

---
+++ gradebook/graderunner.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class graderunner{
+  public static void main(String[] args){
+    Course physics = new Course();
+    physics.addAssignment("Assignment 1");
+    physics.addStudent("idris034");
+    physics.addStudent("fidi1901");
+    physics.assignGrade("idris034", "Assignment 1", 100);
+    physics.assignGrade("idris034", "Assignment 2", 97);
+    physics.assignGrade("fidi1901", "Assignment 1", 95);
+    for(String name: physics.hash.keySet()){
+      HashMap<String, Integer> value = physics.hash.get(name);
+      System.out.println(name + " : " + value);
+    }
+    System.out.println(physics.getGradeAvg());
+  }
+}
